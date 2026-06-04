17ed4fc1995e719001f91e5d086d2027e6331dea
unknown <emad.m.alnagar@gmail.com>
2026-06-04 20:52:56 +0200

    Craete new user controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUser } from './dtos/new-user.dto';
+import { UsersService } from './users.service';
+import { UserRole } from './enums/user-role.enum';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUser) {
+    return this.usersService.create({
+      ...dto,
+      role: dto.role as UserRole,
+    });
+  }
+}
