ad415a4284761f118f8ca060a3a1e5a18ec23673
unknown <emad.m.alnagar@gmail.com>
2026-06-05 17:33:38 +0200

    get all users controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUser } from './dtos/new-user.dto';
 import { UsersService } from './users.service';
 import { UserRole } from './enums/user.enums';
@@ -13,5 +13,10 @@ export class UsersController {
       ...dto,
       role: dto.role as UserRole,
     });
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
   }
 }
