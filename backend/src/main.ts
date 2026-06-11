e31183dca97dfb7d11e9fc95c09e2f4503846fbf
unknown <emad.m.alnagar@gmail.com>
2026-06-11 23:18:26 +0200

    Enable cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
