0daadc8c07a6b413404fd319fe6ca1f76d46e9ed
unknown <emad.m.alnagar@gmail.com>
2026-06-13 03:40:17 +0200

    Fixing backend bugs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import cookieParser = require('cookie-parser');
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
