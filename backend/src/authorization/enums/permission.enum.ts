0540054ac1d478a9503335521ecbad90b5bc8b9b
unknown <emad.m.alnagar@gmail.com>
2026-06-30 21:05:45 +0200

    permission enums

---
+++ backend/src/authorization/enums/permission.enum.ts
@@ -0,0 +1,28 @@
+export enum Permissions {
+  // Rooms
+  CREATE_ROOM = 'create_room',
+  UPDATE_ROOM = 'update_room',
+  DELETE_ROOM = 'delete_room',
+  VIEW_ROOM = 'view_room',
+  // Room Availability
+  MARK_ROOM_AS_AVAILABLE = 'mark_room_as_available',
+  MARK_ROOM_AS_UNAVAILABLE = 'mark_room_as_unavailable',
+  // Roles
+  ASSIGN_ROLE = 'assign_role',
+  // Users
+  CREATE_USER = 'create_user',
+  UPDATE_USER = 'update_user',
+  DELETE_USER = 'delete_user',
+  VIEW_USER = 'view_user',
+  BLOCK_USER = 'block_user',
+  // Bookings
+  CREATE_BOOKING = 'create_booking',
+  UPDATE_BOOKING = 'update_booking',
+  DELETE_BOOKING = 'delete_booking',
+  VIEW_BOOKING = 'view_booking',
+  // Comments
+  CREATE_COMMENT = 'create_comment',
+  UPDATE_COMMENT = 'update_comment',
+  DELETE_COMMENT = 'delete_comment',
+  VIEW_COMMENT = 'view_comment',
+}
