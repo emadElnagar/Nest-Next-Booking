e3c8a574ecfbcaccd7f3ed0cc2d688c70d0cace7
unknown <emad.m.alnagar@gmail.com>
2026-07-10 22:48:01 +0200

    User login and register types

--- frontend/types/user.ts
+++ frontend/types/user.ts
@@ -8,3 +8,15 @@ export interface User {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface UserLogin {
+  email: string;
+  password: string;
+}
+
+export interface UserRegister {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
