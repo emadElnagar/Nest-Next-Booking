5b51e820e96a11ee879c2f5eb09822812e18fe27
unknown <emad.m.alnagar@gmail.com>
2026-07-14 21:26:52 +0200

    Update api to send header

--- frontend/lib/services/api.ts
+++ frontend/lib/services/api.ts
@@ -1,11 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { RootState } from "../store";
 
 export const api = createApi({
   reducerPath: "api",
+
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_URL,
     credentials: "include",
+
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.accessToken;
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
+
   tagTypes: ["Users", "Auth"],
   endpoints: () => ({}),
 });
