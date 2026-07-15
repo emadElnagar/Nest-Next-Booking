aed7bad015352eaff362226f6c0556518080f502
unknown <emad.m.alnagar@gmail.com>
2026-07-15 23:16:23 +0200

    Call users to store

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./services/api";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
   });
 };
 
