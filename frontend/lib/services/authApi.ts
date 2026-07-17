2c13a070d4af155a90868c90d1c2166dbbfc500c
unknown <emad.m.alnagar@gmail.com>
2026-07-17 18:53:41 +0200

    User login query

--- frontend/lib/services/authApi.ts
+++ frontend/lib/services/authApi.ts
@@ -1,13 +1,23 @@
 import { api } from "./api";
+import { UserRegister } from "../../types/user";
 
 export const usersApi = api.injectEndpoints({
   endpoints: (builder) => ({
     // User registration
     registerUser: builder.mutation({
-      query: (user) => ({
+      query: (user: UserRegister) => ({
         url: "/auth/register",
         method: "POST",
         body: user,
+      }),
+    }),
+
+    // User login
+    loginUser: builder.mutation({
+      query: (credentials: { email: string; password: string }) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: credentials,
       }),
     }),
   }),
