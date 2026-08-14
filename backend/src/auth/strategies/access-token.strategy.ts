263d79f16503e6c6a4abb65c8a12ef54e95c6d9a
unknown <emad.m.alnagar@gmail.com>
2026-08-14 19:32:20 +0200

    Fix JWT strategy

--- backend/src/auth/strategies/access-token.strategy.ts
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.accessToken,
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.ACCESS_SECRET,
     });
