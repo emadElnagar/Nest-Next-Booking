b689b26e094e441968772b363e687ccfd6931b3f
unknown <emad.m.alnagar@gmail.com>
2026-09-16 22:21:47 +0200

    Create room images upload decorator

---
+++ backend/src/rooms/decorators/room-images-upload.decorator.ts
@@ -0,0 +1,28 @@
+import { UseInterceptors, BadRequestException } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+export function UseRoomImagesUpload() {
+  return UseInterceptors(
+    FilesInterceptor('images', 6, {
+      storage: diskStorage({
+        destination: './uploads/rooms',
+        filename: (_req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+        },
+      }),
+      fileFilter: (_req, file, callback) => {
+        if (!file.mimetype.match(/\/(jpg|jpeg|png|webp)$/)) {
+          return callback(
+            new BadRequestException('Only image files are allowed!'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  );
+}
